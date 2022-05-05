e03a9c130958cd2ce40f3d8426a3244f2dc8932d
Steve Beyer <beyer_steve@outlook.de>
2022-05-05 15:16:58 +0200

    03 some cleanup

--- 03-programming-concepts/src/main.rs
+++ 03-programming-concepts/src/main.rs
@@ -17,10 +17,10 @@ fn main() {
     // println!("{}", x); // error
  
     // // this will because x is explicitly mutable
-    // let mut x = 5;
-    // println!("{}", x); // 5
-    // x = 6;
-    // println!("{}", x); // 6
+    let mut a = 5;
+    println!("{}", a); // 5
+    a = 6;
+    println!("{}", a); // 6
 
     /*
     Constants
@@ -29,17 +29,22 @@ fn main() {
     // // - are immutable
     // // - must have a type annotation
     // // - can only use constant expressions as values, not computed ones
-    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    // println!("{}", THREE_HOURS_IN_SECONDS); // 10800
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("{}", THREE_HOURS_IN_SECONDS); // 10800
 
     /*
     Shadowing
     */
-    // let x = 5;
-    // let x = x + 1; // shadowed 
-    // {
-    //     let x = x * 2; // shadowed but only in this scope
-    //     println!("{}", x); // 12
-    // }
-    // println!("{}", x); // 6
+    let x = 5;
+    let x = x + 1; // shadowed 
+    // block scope 
+    {
+        let x = x * 2; // shadowed but only in this scope
+        println!("{}", x); // 12
+
+        // end of scope x = 6 again
+    }
+    println!("{}", x); // 6
+
+    
 }
