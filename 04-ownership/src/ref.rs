bbe486b70905923512dfafad623124bddac1cb43
stvbyr <beyer_steve@outlook.de>
2022-05-06 21:36:26 +0200

    04 references

--- 04-ownership/src/ref.rs
+++ 04-ownership/src/ref.rs
@@ -5,15 +5,25 @@ fn main() {
 
     println!("{}", length);
 
-    // non mutable reference cannot be modified 
+    // non mutable reference cannot be modified
     // mutable references can
     let mut string2 = String::from("world");
 
-    // there can only be one mutable reference to one piece of data
     add_punctuation(&mut string2);
+
+    // there can only be one mutable reference to one piece of data
+    let mut s = String::from("hello");
+
+    // we can't make reference here because we already have a mutable reference in this scope
+
+    {
+        let r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s;
 }
 
-fn calc_length(s: &String) -> usize{
+fn calc_length(s: &String) -> usize {
     s.len()
 }
 
