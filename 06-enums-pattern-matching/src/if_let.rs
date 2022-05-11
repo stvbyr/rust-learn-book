1d3bab3e2146a22794645eefaac0eb430597cd8d
stvbyr <beyer_steve@outlook.de>
2022-05-11 11:01:37 +0200

    06 if let

---
+++ 06-enums-pattern-matching/src/if_let.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let config = Some(3);
+    if let Some(max) = config {
+        println!("The maximum is configured to be {}", max)
+    }
+
+    let coin = Coin::Penny;
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+
+    println!("{}", count);
+}
