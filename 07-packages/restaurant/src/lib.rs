8480d3bdb4949704772c915240b6fdd404ad89df
Steve Beyer <beyer_steve@outlook.de>
2022-05-11 15:48:34 +0200

    07 super

--- 07-packages/restaurant/src/lib.rs
+++ 07-packages/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -12,6 +12,31 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        // with super we can reference the level of the parent namespace relative to this one
+        // it's like doing a .. but for paths
+        // the parent lives is the module 'crate', so the root 
+        // this is why we can access the server_order function because it is also in the
+        // module crate
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+fn serve_order() {}
+
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
 
 #[cfg(test)]
